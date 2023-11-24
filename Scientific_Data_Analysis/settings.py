1991f760bc6d1e9d9e0c86d0846fc1214426c9c1
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2023-11-25 00:18:18 +0100

    set up settings

--- Scientific_Data_Analysis/settings.py
+++ Scientific_Data_Analysis/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'Scientific_Data_Analysis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
