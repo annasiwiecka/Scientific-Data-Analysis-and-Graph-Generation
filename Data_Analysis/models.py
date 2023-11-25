93311cfaeb3f11867ba422cb5a968a911cfb1b54
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2023-11-25 03:23:55 +0100

    create model for data

--- Data_Analysis/models.py
+++ Data_Analysis/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class DataModel(models.Model):
+    experiment_name = models.CharField(max_length=100)
+    measurement_value = models.FloatField()
+    timestamp = models.DateTimeField(db_index=True)
+
+    def __str__(self):
+        return self.experiment_name
+    
